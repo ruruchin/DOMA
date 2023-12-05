a36c3075685bf2a18aba82a4e3538282a959df59
ruruchin <konstantinzorenko3@gmail.com>
2023-12-05 14:32:08 +0300

    тут моя жизнь

---
+++ PZ_6/PZ_6.3.py
@@ -0,0 +1,9 @@
+def Index_Spiska(lsd):
+    element = int(input())
+    lsd = [int(input(f"Введите число{i+1}") for i in range(element))]
+    K = int(input("Введите число сдвигов K: "))
+    i = 0
+    for i in range(K):
+        first = lsd[i]
+        second = lsd[i+1]
+
---
+++ PZ_7/PZ_7.2.py
@@ -0,0 +1,12 @@
+predlo = str(input())
+for word in predlo:
+    if len(predlo) > len(word):
+        predlo = word
+        print(word)
+
+
+
+
+
+
+
